00ceeb9fcbc40d322fb200988995f7cb6aad7dd1
Julia Jubileu <juliajubileu@gmail.com>
2021-01-25 16:38:50 -0200

    Adiciona paginas para visualizaçao das promoçoes

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -2,4 +2,7 @@ class PromotionsController < ApplicationController
     def index
         @promotions = Promotion.all
     end
+    def show
+        @promotion = Promotion.find(params[:id])
+    end
 end
