b1373af765bf7d040bf81300ad904d5e90424a58
Julia Jubileu <juliajubileu@gmail.com>
2021-02-05 18:02:55 -0200

    Admin gera cupons

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -1,37 +1,35 @@
 class PromotionsController < ApplicationController
-    def index
-        @promotions = Promotion.all
-    end
+  def index
+    @promotions = Promotion.all
+  end
 
-    def show
-        @promotion = Promotion.find(params[:id])
-    end
+  def show
+    @promotion = Promotion.find(params[:id])
+  end
 
-    def new
-        @promotion = Promotion.new
-    end
+  def new
+    @promotion = Promotion.new
+  end
 
-    def create
-        @promotion = Promotion.new(promotion_params)
+  def create
+   @promotion = Promotion.new(promotion_params)
+   if @promotion.save
+     redirect_to @promotion
+   else
+     render 'new'
+   end
+  end
 
-        if @promotion.save
-            redirect_to @promotion
-        else
-            render 'new'
-        end
-    end
-
-    def generate_coupons
-        @promotion = Promotion.find(params[:id])
-        @promotion.generate_coupons!
-        end
-        redirect_to @promotion, notice: t('.success')
-    end
+  def generate_coupons
+    @promotion = Promotion.find(params[:id])
+    @promotion.generate_coupons!
+    redirect_to @promotion, notice: t('.success')
+  end
 
     private
-    def promotion_params
-        params.require(:promotion).permit(:name, :description, :code, 
-                                          :discount_rate, :coupon_quantity, 
-                                          :expiration_date)
-    end
+  def promotion_params
+    params.require(:promotion).permit(:name, :description, :code, 
+                                       :discount_rate, :coupon_quantity, 
+                                       :expiration_date)
+  end
 end
