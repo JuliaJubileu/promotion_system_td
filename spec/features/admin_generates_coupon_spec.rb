8211d3a29e2ba6621ab5e12f64adb62ac5677fd2
Julia Jubileu <juliajubileu@gmail.com>
2021-02-10 12:43:56 -0200

    Teste - esconder botao apos gerar cupons

--- spec/features/admin_generates_coupon_spec.rb
+++ spec/features/admin_generates_coupon_spec.rb
@@ -18,7 +18,7 @@ feature 'Admin generates coupons' do
 
     visit root_path
     click_on 'Promoções'
-    click_on promotion.name
+    click_on 'Natal'
     click_on 'Gerar cupons'
 
     expect(current_path).to eq(promotion_path(promotion))
@@ -26,10 +26,19 @@ feature 'Admin generates coupons' do
     expect(page).to have_content('NATAL10-0001')
     expect(page).to have_content('NATAL10-0002')
     expect(page).to have_content('NATAL10-0100')
-    expect(page).not_to have_content('NATAL10-0101')
+    expect(page).to_not have_content('NATAL10-0101')
   end
 
-  # TODO: fazer esse teste
-  xscenario 'hide button if coupons generated' do
+  scenario 'hide button if coupons generated' do
+    user = User.create!(email: 'julia@dev.com', password: '123456')
+    promotion = Promotion.create!(name: 'Natal', description: 'Promoção de Natal',
+                      code: 'NATAL10', discount_rate: 10, coupon_quantity: 100,
+                      expiration_date: '22/12/2033', user: user)
+
+    login_as user, scope: :user
+    promotion.coupons.create!
+    visit promotion_path(promotion)
+
+    expect(page).to_not have_link('Gerar cupons')
   end
 end
