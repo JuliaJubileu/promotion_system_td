c0f1d6049ebcf40c695744fbe3e50866848d4d7b
Julia Jubileu <juliajubileu@gmail.com>
2021-02-10 16:47:42 -0200

    Admin realiza busca e vê mensagem de erro caso a busca falhe

--- spec/features/admin_searches_promotion_spec.rb
+++ spec/features/admin_searches_promotion_spec.rb
@@ -12,10 +12,7 @@ feature 'User searches for promotion' do
     first_promotion = Promotion.create!(name: 'Natal', description: 'Promoção de Natal',
                                code: 'NATAL10', discount_rate: 10, coupon_quantity: 100,
                                expiration_date: '22/12/2033', user: user)
-    second_promotion = Promotion.create!(name: 'Ano Novo', description: 'Promoção de Ano Novo',
-                               code: 'NOVO22', discount_rate: 5, coupon_quantity: 40,
-                               expiration_date: '02/01/2022', user: user)
-    third_promotion = Promotion.create!(name: 'Aniversário', description: 'Promoção de aniversário',
+    second_promotion = Promotion.create!(name: 'Aniversário', description: 'Promoção de aniversário',
                                code: 'NIVER50', discount_rate: 50, coupon_quantity: 50,
                                expiration_date: '25/12/2022', user: user)
 
@@ -29,5 +26,23 @@ feature 'User searches for promotion' do
     expect(page).to have_content('NATAL10')
     expect(page).not_to have_content('Aniversário')
     expect(page).not_to have_content('Promoção de Ano Novo')
+  end
+  scenario 'and sees error message when search fails' do
+    user = User.create!(email: 'julia@dev.com', password: '123456')
+    first_promotion = Promotion.create!(name: 'Natal', description: 'Promoção de Natal',
+                               code: 'NATAL10', discount_rate: 10, coupon_quantity: 100,
+                               expiration_date: '22/12/2033', user: user)
+    second_promotion = Promotion.create!(name: 'Aniversário', description: 'Promoção de aniversário',
+                               code: 'NIVER50', discount_rate: 50, coupon_quantity: 50,
+                               expiration_date: '25/12/2022', user: user)
+
+    login_as user, scope: :user
+    visit root_path
+    fill_in 'Busca:', with: 'Ano Novo'
+    click_on 'Pesquisar'
+
+    expect(current_path).to eq search_path
+    expect(page).to have_content('Nenhuma promoção encontrada')
+    expect(page).to have_link('Voltar')
   end
 end
