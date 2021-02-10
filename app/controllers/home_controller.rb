8d669326633666083e786119497f38c20e6f3824
Julia Jubileu <juliajubileu@gmail.com>
2021-02-10 15:59:11 -0200

    Admin busca promoçao

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
     def index
     end
+
+    def search
+        @promotions = Promotion.where('name like ? OR description like ? OR code like ?', 
+                                      params[:q], params[:q], params[:q])
+    end
 end 
---
+++ spec/features/admin_searches_promotion_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+feature 'User searches for promotion' do
+  scenario 'must be signed in' do
+    visit root_path
+    click_on 'Promoções'
+
+    expect(current_path).to eq new_user_session_path
+  end
+  scenario 'successfully' do
+    user = User.create!(email: 'julia@dev.com', password: '123456')
+    first_promotion = Promotion.create!(name: 'Natal', description: 'Promoção de Natal',
+                               code: 'NATAL10', discount_rate: 10, coupon_quantity: 100,
+                               expiration_date: '22/12/2033', user: user)
+    second_promotion = Promotion.create!(name: 'Ano Novo', description: 'Promoção de Ano Novo',
+                               code: 'NOVO22', discount_rate: 5, coupon_quantity: 40,
+                               expiration_date: '02/01/2022', user: user)
+    third_promotion = Promotion.create!(name: 'Aniversário', description: 'Promoção de aniversário',
+                               code: 'NIVER50', discount_rate: 50, coupon_quantity: 50,
+                               expiration_date: '25/12/2022', user: user)
+
+    login_as user, scope: :user
+    visit root_path
+    fill_in 'Busca:', with: 'Natal'
+    click_on 'Pesquisar'
+
+    expect(current_path).to eq search_path
+    expect(page).to have_content('Natal')
+    expect(page).to have_content('NATAL10')
+    expect(page).not_to have_content('Aniversário')
+    expect(page).not_to have_content('Promoção de Ano Novo')
+  end
+end
